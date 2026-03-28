256bcecf26ce34a8c839f454f78f9a65c2818c38
samuido <rebuild.up.up@gmail.com>
2026-03-28 16:15:10 +0900

    fix: commit prebuilt wasm bridge artifacts

---
+++ src/wasm/tastile-core-wasm/pkg/tastile_core_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmcoreengine_free: (a: number, b: number) => void;
+export const wasmcoreengine_execute: (a: number, b: number, c: number) => [number, number];
+export const wasmcoreengine_new: () => number;
+export const wasmcoreengine_read_snapshot_json: (a: number, b: number, c: number) => [number, number, number, number];
+export const Java_app_tastile_android_core_TastileCoreBridge_00024JniNativeBindings_dispatchCommand: (a: number, b: number, c: number) => number;
+export const Java_app_tastile_android_core_TastileCoreBridge_00024JniNativeBindings_getSnapshot: (a: number, b: number) => number;
+export const Java_app_tastile_android_core_TastileCoreBridge_00024JniNativeBindings_replaceEventLog: (a: number, b: number, c: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
